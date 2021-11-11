ee771b9d6a1796e4c8b25408f4e4495dee5e2099
Ousmana Traore <traoreosman@yahoo.fr>
2021-11-11 20:26:25 +0100

    escalier

---
+++ PYthon/POO/2_programme_tortue/main.py
@@ -0,0 +1,20 @@
+import turtle
+
+# Definition de fonction d'escalier
+def escalier(taille, nb):
+
+    for i in range(0, nb):
+        t.forward(taille)
+        t.left(90)
+        t.forward(taille)
+        t.right(90)
+        taille -= 10
+    t.forward(taille)
+
+t = turtle.Turtle()
+
+
+escalier(60,5)
+
+
+turtle.done()
