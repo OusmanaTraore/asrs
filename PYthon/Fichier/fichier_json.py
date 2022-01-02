e712dd247e6b713d2c2781eed4b4cccb8663687c
OusmanaTraore <traoreosman@yahoo.fr>
2022-01-02 13:47:32 +0100

    fichier JSON , fichier txt conversion

--- PYthon/Fichier/fichier_json.py
+++ PYthon/Fichier/fichier_json.py
@@ -1,17 +1,26 @@
-import os.path
+import json
 
-if not os.path.exists("rep"):
-    os.mkdir("rep") # création de repertoire
-    # os.rmdir("rep") supprimer un répertoire
 
-filename =os.path.join("rep", "mon_fichier.txt")
-if os.path.exists(filename):
-    print(filename ,'existe')
-    f = open(filename,'r')
-    texte= f.read()
-    print(texte)
-    f.close
-else:
-    print(filename, "n'existe pas")
-    f = open(filename,'w')  # création de fichier
-    # os.remove(filename) supprimer un fichier
+personne = {
+    "nom": "Paul",
+    "age": 25,
+    "amis":["Sophie", "Marc","Jean"]
+}
+
+# ====Conversion au format JSON ======
+personne_json= json.dumps(personne)
+print("JSON:" + personne_json)
+
+f =open("fichier_json.txt", "w")
+f.write(personne_json)
+f.close()
+#=====================================
+
+# ====Conversion au format txt ======
+f =open("fichier_json.txt", "r")
+donnees_json=f.read()
+f.close
+
+personne= json.loads(donnees_json)
+print("NOM:" + personne["nom"])
+#=====================================
