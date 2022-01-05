3ee8fb97b1edffbef950b1d5a2f9a826defa890b
OusmanaTraore <traoreosman@yahoo.fr>
2022-01-06 00:09:47 +0100

    fichier zip

---
+++ PYthon/Fichier/fichier_zip.py
@@ -0,0 +1,12 @@
+
+import zipfile
+import shutil
+
+"""fichier_zip =zipfile.ZipFile("fichiers_excel.zip", "w",zipfile.ZIP_DEFLATED)
+fichier_zip.write('octobre.xlsx')
+fichier_zip.write('novembre.xlsx')
+fichier_zip.write('decembre.xlsx')
+fichier_zip.close()"""
+
+
+shutil.make_archive("total_ventes_trimestre" , "zip" , "ventes")
