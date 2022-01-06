842b685342ee3eeb75c57c80f59828bcb54d6e2f
OusmanaTraore <traoreosman@yahoo.fr>
2022-01-06 02:28:48 +0100

    request .jpg

--- PYthon/Fichier/programme_requests.py
+++ PYthon/Fichier/programme_requests.py
@@ -16,3 +16,14 @@ if reponse.status_code == 200:
     print(reponse.text)
 else:
     print("ERREUR code: " +  str(reponse.status_code))
+
+
+
+reponse = requests.get("https://codeavecjonathan.com/res/papillon.jpg")
+if reponse.status_code == 200:
+    f = open("papillon.jpg","wb")
+    f.write(reponse.content)
+    f.close()
+    print("Ecriture terminée")
+else:
+    print("ERREUR code: " +  str(reponse.status_code))    
